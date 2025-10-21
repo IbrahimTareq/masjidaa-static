cd6c5db5654b5fa9bed899191d5d59efa5a1f6ee
Ibrahim <ibrahimtareq@live.com>
2025-10-21 16:23:59 +1100

    fix: Simple layout rounded edges

--- app/masjid/[id]/layout/prayer-screens/[type]/prayer-screens.tsx
+++ app/masjid/[id]/layout/prayer-screens/[type]/prayer-screens.tsx
@@ -7,7 +7,7 @@ export default function PrayerScreens({
 }) {
   return (
     <div className="h-screen p-2 sm:p-4 lg:p-5 bg-gradient-to-br from-theme to-theme flex flex-col">
-      <div className="flex-1 w-full rounded-t-2xl sm:rounded-t-3xl overflow-hidden">
+      <div className="flex-1 w-full rounded-t-2xl rounded-b-2xl sm:rounded-t-3xl sm:rounded-b-3xl overflow-hidden">
         {children}
       </div>
     </div>
